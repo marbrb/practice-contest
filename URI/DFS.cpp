beebd73878da5afe4e1b6e969f4dd01280a8fa62
Miguel Rodriguez <marbrb1@gmail.com>
2018-01-08 21:41:49 -0500

    removed utility files

--- URI/DFS.cpp
+++
@@ -1,7 +0,0 @@
-void dfs(int node, bool *vis, vi *adj) {
-    vis[node] = true;
-    for(int i=0; i<adj[node].size(); i++) {
-        if (!adj[node][i])
-          dfs(adj[node][i], vis, adj);
-    }
-}
